fbcfb53a865b41533d7e50d688b58d079b8df4c7
borodulin <borodulin@gmail.com>
2018-01-23 17:11:32 +0500

    tests

--- tests/_data/test-local.php
+++ tests/_data/test-local.php
@@ -3,4 +3,9 @@
 return [
     'id' => 'test-api',
     'basePath' => dirname(__DIR__),
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(__DIR__) . '/vendor',
 ];
